1dfbf6fdf5b921bd91c585a60df4caa969b5f1fd
chandan <chandankumar348832@gmail.com>
2022-10-04 17:53:02 +0530

    c

---
+++ Building web/web 11/app.js
@@ -0,0 +1,11 @@
+window.onresize = screen;
+window.onload = screen;
+
+
+function screen () {
+    myWidth = window.innerWidth;
+
+    document.getElementById('size').innerHTML 
+    = "width : " + myWidth + "px";
+   
+}
